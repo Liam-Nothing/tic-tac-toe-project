981c3cb7d153e128c1aec0b22f356906514d79dd
Liam-Nothing <git.pointy033@passmail.net>
2025-02-13 18:58:45 +0100

    WIP: Remove front

--- client/src/pages/Home.js
+++
@@ -1,8 +0,0 @@
-const Home = () => (
-    <div className="p-8">
-        <h1 className="text-2xl font-bold">Accueil</h1>
-        <p>Bienvenue sur notre application.</p>
-    </div>
-);
-
-export default Home;
